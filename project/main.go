9f4984c6b08b3f22ca4394abb68daea55b1e299d
Daan Selen <selendaan@hotmail.com>
2022-06-14 15:54:39 +0200

    Change added confirm function and some handy functions

--- project/main.go
+++ project/main.go
@@ -6,7 +6,11 @@ import (
 	"strings"
 )
 
-var ()
+var (
+	selectedIndex = 0
+	urlArray      = [2]string{"https://powerdns.systemec.nl/rest/rest.php", "https://teslatest.systemec.nl/rest/rest.php"}
+	selectedURL   = urlArray[selectedIndex]
+)
 
 func main() {
 	printMainStartScreen()
@@ -15,18 +19,22 @@ func main() {
 
 func printMainStartScreen() {
 	printLines()
+	fmt.Println()
 	fmt.Println("Welcome to the PDNSAPIReader.")
 	fmt.Println()
-	fmt.Println("Press any key to continue...")
 	printLines()
-	fmt.Scanln()
+	pressAny()
 }
 
 func printMainCommandScreen() {
+
 	printLines()
+	fmt.Println()
 	fmt.Println("Available commands:")
 	fmt.Println()
-	fmt.Print("SearchDomain (Retrieves a list of all domains that fit the searchkey)\nShowDomain (Retrieves all Records for a given domain)\nsetTTL (Set Time To Live)\n\nExit (Immediatly exit the program)")
+	fmt.Println("COMMANDS:\nSearchDomain (Retrieves a list of all domains that fit the searchkey)\nShowDomain (Retrieves all Records for a given domain)\nsetTTL (Set Time To Live)\n\nExit (Immediatly exit the program)")
+	fmt.Println()
+	fmt.Println("CLIENT SETTINGS MANAGEMENT:\nURLChange (Option to change between experimental and production API Servers)\nURLStatus (Checks which environment it's going to call (Default: Production))")
 	fmt.Println()
 	printLines()
 	fmt.Print("Enter the command you want: ")
@@ -39,25 +47,98 @@ func selectInput(trialCommand string) {
 	fmt.Println()
 	switch trialCommand {
 	case "searchdomain":
-		printLines()
+		searchDomain()
 	case "showdomain":
-		printLines()
+		showDomain()
 	case "setttl":
-		printLines()
+		setTTL()
 	case "exit":
 		os.Exit(0)
-	default:
-		printMainCommandScreen()
+	case "urlchange", "uc":
+		urlChange()
+	case "urlstatus", "us":
+		urlStatus()
 	}
+	printMainCommandScreen()
 }
 
 func searchDomain() {
-
+	printLines()
+	fmt.Println()
+	fmt.Print("What is your searchkey? ")
+	var searchKeyInput string
+	fmt.Scanln(&searchKeyInput)
+	searchKey := confirm(searchKeyInput)
+	fmt.Println(searchKey)
+	fmt.Println()
+	printLines()
 }
 
 func showDomain() {
+	printLines()
+	fmt.Println()
+
+	fmt.Println()
+	printLines()
+}
+
+func setTTL() {
+	printLines()
+	fmt.Println()
+
+	fmt.Println()
+	printLines()
+}
+
+func confirm(trialInput string) string {
+	printLines()
+	fmt.Println()
+	fmt.Print("Your input is:", trialInput)
+	fmt.Println()
+	printLines()
+	fmt.Print("is this correct? (Y/n) ")
+	var confirmInput string
+	fmt.Scanln(&confirmInput)
+	if strings.ToLower(confirmInput) == "n" {
+		fmt.Print("Type in the correct input: ")
+		var correctedInput string
+		fmt.Scanln(&correctedInput)
+		return correctedInput
+	} else {
+		return trialInput
+	}
+}
+
+func urlChange() {
+	if selectedIndex == 0 {
+		selectedURL = urlArray[1]
+		selectedIndex = 1
+	} else {
+		selectedURL = urlArray[0]
+		selectedIndex = 0
+	}
+	printLines()
+	fmt.Println()
+	fmt.Println("URL CHANGE SUCCES. Current:", selectedURL)
+	fmt.Println()
+	printLines()
+	pressAny()
+}
+
+func urlStatus() {
+	printLines()
+	fmt.Println()
+	fmt.Println("Current URL:", selectedURL)
+	fmt.Println()
+	printLines()
+	pressAny()
 }
 
 func printLines() {
 	fmt.Println(strings.Join([]string{strings.Repeat("=", 80)}, " "))
+}
+
+func pressAny() {
+	fmt.Println("Press any key to continue...")
+	fmt.Scanln()
 }
